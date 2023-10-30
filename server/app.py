7ea6e93f477edbc2cc27463bd3758933a9367af6
Collin <shakescollin@aol.com>
2023-10-30 14:09:55 -0400

    first push with models and db data

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
-
+from models import *
 # Local imports
 from config import app, db, api
 # Add your model imports
@@ -16,6 +16,7 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
 
 
 if __name__ == '__main__':
